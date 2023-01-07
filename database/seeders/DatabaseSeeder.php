5cf88252a9278350b96fed87d03a4c71885ec86d
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2023-01-07 21:22:55 +0800

    Create DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::insert([
+            'name' => 'Super Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+
+    }
+}
