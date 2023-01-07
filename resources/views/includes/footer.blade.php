37ae6ee57db2413435e119772c5025e47ef29afa
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2023-01-07 23:18:10 +0800

    Create footer.blade.php

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,35 @@
+ <!--Footer Bottom-->
+ <div class="footer-bottom">
+    <div class="auto-container">
+        <div class="row clearfix">
+            <div class="column col-md-3 col-sm-12 col-xs-12">
+                <div class="logo">
+                    <a href="{{ asset('public/assets/images/logo.png') }}"rel="vbi logo"></a>
+                </div>
+            </div>
+            <div class="column col-md-6 col-sm-12 col-xs-12">
+                <div class="text">Sample Description</div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="copyright-section">
+        <div class="auto-container">
+            <div class="row clearfix">
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <ul class="footer-nav">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Advertisement</a></li>
+                        <li><a href="#">Contacts</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <div class="copyright">&copy; Copyright CJMM</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
