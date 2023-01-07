380ed0f90dfe5d09f9e4d3b1b04f3e73916b41c0
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2023-01-07 23:18:42 +0800

    Add files via upload

---
+++ resources/views/includes/mainHome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+@include('includes.header')
+
+
+
+    @yield('content')
+
+@include('includes.script')
+
+
---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,23 @@
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon fa fa-angle-double-up"></span></div>
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon fa fa-angle-double-up"></span></div>
+
+<!--End Scroll to top-->
+
+<script src="{{ asset('public/assets/js/jquery.js') }}"></script>
+<script src=" {{ asset( 'public/assets/js/bootstrap.min.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/jquery.fancybox.pack.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/jquery.fancybox-media.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/owl.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/appear.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/wow.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/jquery.mCustomScrollbar.concat.min.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/script.js') }} "></script>
+<script src=" {{ asset( 'public/assets/js/color-settings.js') }} "></script>
+
+<!-- Disqus Comment-->
+<script id="dsq-count-scr" src="//ehssportal.disqus.com/count.js" async></script>
+</body>
+</html>
